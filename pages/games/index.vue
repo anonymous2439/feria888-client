bae7581efc9f34c532e83160c653421e0eb61915
anonymous <mranonymous2439@gmail.com>
2023-05-28 20:28:25 +0800

    changes

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -10,6 +10,5 @@
 </template>
 
 <style>
-    .games-page-con{padding: 40px 0;}
-    .category-name{ text-align: center; font-size: 40px; text-transform: uppercase; margin-bottom: 40px; } 
+    .category-name{ text-align: center; font-size: 40px; text-transform: uppercase; margin: 40px 0; } 
 </style>

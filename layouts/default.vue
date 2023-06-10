4ee679893945dcb0e348e518fee5cb025e2905f9
anonymous <mranonymous2439@gmail.com>
2023-06-10 22:25:27 +0800

    changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="app">
 
-        <div class="app-con" v-show="is_mounted">
+        <div class="app-con" v-show="is_mounted" :class="{ animate_out : is_mounted }">
             <AppHeader />        
     
             <div class="app-content">        
@@ -16,12 +16,22 @@
 <script setup>
     const is_mounted = useState('is_mounted', () => false)
     onMounted(() => {
-        // Your callback function here
         is_mounted.value = true
     });
 </script>
 
 <style scoped>
+
+    @keyframes ease {
+    from {opacity: 0%;}
+    to {opacity: 100%;}
+    }
+
+    .animate_out {
+        animation-name: ease;
+        animation-duration: 2s;
+    }
+
     .app{position: relative;}
     .app:before { content: ""; background: url('/images/hidden_treasure.jpg'); background-position-x: 0%; background-position-y: 0%; background-repeat: repeat; background-size: auto; position: fixed; top: 0; left: 0; height: 100vh; width: 100%; filter: brightness(0.6); background-repeat: no-repeat; background-size: cover; background-position-y: center; background-position-x: center; } 
 

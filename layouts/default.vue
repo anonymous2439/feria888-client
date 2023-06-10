5deec570e24a31f0fc6f660c2c09cdebefbd680e
anonymous <mranonymous2439@gmail.com>
2023-06-10 20:18:06 +0800

    changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="app">
-        <transition name="fade">
-        <div class="app-con">
+
+        <div class="app-con" v-show="is_mounted">
             <AppHeader />        
     
             <div class="app-content">        
@@ -9,7 +9,7 @@
             </div>
             <AppFooter />
         </div>
-        </transition>
+
     </div>
 </template>
 
@@ -18,13 +18,11 @@
     onMounted(() => {
         // Your callback function here
         is_mounted.value = true
-        // console.log('Component has been rendered!');
     });
 </script>
 
 <style scoped>
     .app{position: relative;}
     .app:before { content: ""; background: url('/images/hidden_treasure.jpg'); background-position-x: 0%; background-position-y: 0%; background-repeat: repeat; background-size: auto; position: fixed; top: 0; left: 0; height: 100vh; width: 100%; filter: brightness(0.6); background-repeat: no-repeat; background-size: cover; background-position-y: center; background-position-x: center; } 
-    
 
 </style>

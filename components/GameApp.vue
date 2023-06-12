19de187371adf1559361fa52d2c085d11bf380e1
anonymous <mranonymous2439@gmail.com>
2023-06-12 13:44:54 +0800

    changes

--- components/GameApp.vue
+++ components/GameApp.vue
@@ -1,36 +1,64 @@
 <template>
 
     <div class="game-app">
-        <a :href="link" target="_blank">
-            <figure><img :src="icon"/></figure>
-            <p>
-                <slot></slot>
-            </p>
-        </a>
-    </div>
+        <div class="game-app-con">
+            <div class="game-app-overlay"></div>
+            <figure>
+                <img :src="icon"/>
+                <div class="game-app-links">
+                    <!-- <a :href="link" target="_blank">Play</a> -->
+                    <a @click="playClick" class="btn-primary">Play</a>
+                </div>
+                <div class="link-mobile" @click="playClick"></div>
+            </figure>            
+        </div>
+        <ModalPlayGame :modal_active="play_active" :icon="props.icon" :link="props.link" :activeModal="modalCallback">
+            <slot />
+        </ModalPlayGame>
+    </div>    
 
 </template>
 
 
 <script setup>
+
     const props = defineProps({
         icon: String,
         link: String,
     })
+
+    const play_active = ref(false)
+
+    function playClick() {
+        play_active.value = true
+    }
+
+    function modalCallback(event) {
+        play_active.value = event
+    }
+
 </script>
 
 <style scoped>
+
     .game-app{max-width: 100%;width: calc((100% - 68px)/5);position: relative;margin-bottom: 25px;}
     .game-app h4{text-align: center;}
     .game-app p { color: #fff; text-overflow: ellipsis;width: 100%;max-width: 100%;overflow: hidden;display: -webkit-box;-webkit-line-clamp: 2;-webkit-box-orient: vertical;} 
-    .game-app a { text-decoration: none;transition: .6s; } 
-    .game-app a:hover { opacity: .5; } 
+    .game-app-con { text-decoration: none;transition: .6s;position: relative;} 
+    .game-app-con:hover .game-app-overlay {opacity: 1;}
+    .game-app-con:hover .game-app-links {opacity: 1;}
+    .game-app-overlay { background: #0000007D;width: 100%;height: 100%;position: absolute; top: 0;left: 0; opacity: 0; transition: .6s;}
+    .game-app-links {position: absolute;left: 0;right: 0;top: 50%;transform: translateY(-50%); opacity: 0;padding: 0 10px; transition: .6s;}
+    .link-mobile {position: absolute;top: 0;left: 0;width: 100%;height: 100%;cursor: pointer; display: none;}
+    .game-app-con a{max-width: 194px;}
 
     @media only screen and (max-width: 1200px) {
         .game-app { width: calc((100% - 85px)/6); } 
     }
     @media only screen and (max-width: 1000px) {
         .game-app {width: calc((100% - 51px)/4);}
+        .game-app-links {display: none;}
+        .link-mobile {display: block;}
     }
     @media only screen and (max-width: 600px) {
         .game-app {width: calc((100% - 17px)/2);}
---
+++ components/modal/PlayGame.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="play-game-modal" v-if="modal_active">
+        <div class="overlay" @click="closeModal"></div>
+        <div class="modal-container">
+            <div class="content">
+                <div class="header">
+                    <h2><slot /></h2>
+                </div>
+                <div class="body">
+                    <img :src="props.icon" />
+                </div>
+                <div class="footer">
+                    <a :href="props.link" target="_blank" class="btn-primary">Play</a>
+                    <a @click="closeModal" class="btn-secondary outline">Cancel</a>                
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        modal_active: Boolean,
+        icon: String,
+        link: String,
+        activeModal: Function,
+    })
+
+    const closeModal = () => {
+        props.activeModal(false)
+    };
+</script>
+
+<style scoped>
+    .play-game-modal {width: 100%;height: 100%;position: fixed;top: 0;left: 0;z-index: 1;}
+    .modal-container {position: absolute;left: 0;right: 0;top: 0;bottom: 0;margin: 0 5%;}
+    .overlay {background: #00000090;position: absolute;top: 0;left: 0;width: 100%;height: 100%;}
+    .content {position: absolute;top: 50%;left: 0;right: 0;max-width: 500px;margin: 0 auto;padding: 40px 15px;background: #fff;color: #1a1a1a;border-radius: 8px;transform: translateY(-55%);}
+    .body {padding: 15px 0;}
+    .footer {display: flex;flex-direction: column;border-top: 1px solid #000;padding: 15px 0 0;row-gap: 12px;}
+    .footer a {max-width: 122px;width: 100%;}
+</style>
